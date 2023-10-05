e8c95e0b58acb15bf9ab28bb0d7725ff0e14766a
dmae97 <146327957+dmae97@users.noreply.github.com>
2023-10-06 01:40:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -172,8 +172,8 @@ ad_code = """
      data-ad-format="auto"
      data-full-width-responsive="true"></ins>
 <script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>"""
+     (adsbygoogle = window.adsbygoogle || []).push({});"""
+</script>
 
 st.markdown(ad_code, unsafe_allow_html=True)
 
